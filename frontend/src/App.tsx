08f0628e1f763b33d04958470b65cba597deea15
Runictable3626 <jaganaathjay@gmail.com>
2025-03-27 19:56:02 -0700

    Added Clerk Authentication along with Sign-In Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,34 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ActionCreationPage from "./pages/ActionCreationPage";
 import ActionDashboardPage from "./pages/ActionDashboardPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Sidebar from "./components/Sidebar";
 import Modal from "react-modal";
+import { useUser } from '@clerk/clerk-react';
 
 Modal.setAppElement("#root");
 
+
+
+
+
 const App = () => {
+  interface ProtectedRouteProps {
+    element: any; // Using 'any' type for the 'element' prop
+  }
+  
+  const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
+    const { isSignedIn } = useUser(); // Clerk hook to check if the user is signed in
+  
+    if (!isSignedIn) {
+      
+      return <Navigate to="/" replace />; // Redirect to sign-in page if not authenticated
+    }
+  
+    return element;
+  };
+
   return (
     <Router>
       <div className="flex">
@@ -19,7 +39,10 @@ const App = () => {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/action-dashboard" element={<ActionDashboardPage />} />
-            <Route path="/create-action" element={<ActionCreationPage />} />
+            <Route 
+              path="/create-action" 
+              element={<ProtectedRoute element={<ActionCreationPage />} />} 
+            />
             <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route */}
           </Routes>
         </div>
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
-import { NavLink, useLocation } from "react-router-dom";
-import { Home, LayoutGrid, PlusCircle, Menu } from "lucide-react";
-import { useState, useEffect } from "react";
+import { NavLink} from "react-router-dom";
 import GlobalAudio from "./GlobalAudio";
+import { SignedIn, SignedOut, SignInButton, UserButton, SignIn } from '@clerk/clerk-react'
+
 
 const Sidebar = () => {
   return (
@@ -12,14 +12,27 @@ const Sidebar = () => {
       <NavLink to="/action-dashboard" end className="nav-link">
       Action Dashboard
       </NavLink>
-      <NavLink to="/create-action" className="nav-link">
-      Create Action
-      </NavLink>
+
+      <SignedIn>
+        <NavLink to="/create-action" className="nav-link">
+        Create Action
+        </NavLink>
+
+        <UserButton />
+      </SignedIn>
+
+      <SignedOut>
+      <SignInButton>
+        <button className="nav-link">Admin Login</button>
+      </SignInButton>
+      </SignedOut>
+
 
       <GlobalAudio/>
+
+
     </nav>
   );
 };
 
 export default Sidebar;
-
