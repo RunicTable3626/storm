690c6846e74b2656223f72e639525cc8361881c4
Runictable3626 <jaganaathjay@gmail.com>
2025-04-11 18:26:47 -0700

    Reordered clerkmiddleware and cors declarations in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,12 +13,14 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI as string;
 
-app.use(clerkMiddleware())
-app.use(express.json());
 app.use(cors({
   origin: process.env.ALLOWED_ORIGIN,
   credentials: true,
 }));
+
+app.use(clerkMiddleware())
+app.use(express.json());
+
 
 // Connect to MongoDB
 const connectDB = async (): Promise<void> => {
